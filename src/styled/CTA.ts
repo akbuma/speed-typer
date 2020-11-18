9e7e980030b933540caec63dfebf6771df40f178
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-11-18 02:49:20 -0800

    styled home page

---
+++ src/styled/CTA.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default styled(Link)`
+  font-size: 1.5rem;
+  text-align: center;
+  border: 1px solid red;
+  display: block;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
