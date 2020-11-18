f82d1f42b1742ecf34158972405c04a55cdffa19
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-11-17 22:48:43 -0800

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Game from './pages/Game';
+import GameOver from './pages/GameOver';
+import HighScores from './pages/HighScores';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+import {Container} from './styled/Container';
+import { Main } from './styled/Main';
+import Global from './styled/Global';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Global />
+      <Main>
+        <Container>
+        <Navbar />
+        <Switch>
+          <Route path="/game" component={Game} />
+          <Route path="/highScores" component={HighScores} />
+          <Route path="/gameOver" component={GameOver} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Container>`
+      </Main>
+    </Router>
   );
 }
 
