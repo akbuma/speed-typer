f63282826185d2de0d2012193a92168bea43bc18
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-11-17 22:59:40 -0800

    add navbar styled component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { StyledNavbar } from '../styled/Navbar';
 
 export default function Navbar() {
     return (
-        <nav>
-            <div>
-                <Link to="/">
-                    Learn.Build.<span>Type.</span>
-                </Link>
-            </div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/highScores">High Scores</Link></li>
-            </ul>
-        </nav>
+        <StyledNavbar>
+                <div>
+                    <Link to="/">
+                        Learn.Build.<span>Type.</span>
+                    </Link>
+                </div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/highScores">High Scores</Link></li>
+                </ul>
+        </StyledNavbar>
     )
 }
