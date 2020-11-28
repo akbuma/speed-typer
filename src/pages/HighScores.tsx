101f56d6e0fc604f46548315684ded3603924365
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-11-28 01:15:01 -1000

    modify HighScores component to retrieve data in sorted order

--- src/pages/HighScores.tsx
+++ src/pages/HighScores.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { ScoresList, ScoreLI } from "../styled/HighScores";
+
+interface ScoreObject {
+  fields: {
+    Score: number;
+    name: string;
+  };
+  id: string;
+}
 
 export default function HighScores() {
-    return(
-        <div>
-            <h1>HighScores</h1>
-        </div>
-    );
+  const [highScores, setHighScores] = useState([]);
+
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch("/.netlify/functions/getHighScores");
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (err) {}
+    };
+    loadHighScores();
+  }, []);
+
+  return (
+    <div>
+      <h1>HighScores</h1>
+      <ScoresList>
+        {highScores.map((score: ScoreObject, idx) => (
+          <ScoreLI key={idx}>
+            {score.fields.name} - {score.fields.Score}
+          </ScoreLI>
+        ))}
+      </ScoresList>
+    </div>
+  );
 }
